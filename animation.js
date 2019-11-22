d5b07414ce7afa45aaa93605e8f4350f3057bf51
maximsobolevskiy <maximsob1233@gmail.com>
2019-11-22 15:30:32 +0300

    feat: implement slider

--- animation.js
+++ animation.js
@@ -9,33 +9,3 @@ burger.addEventListener('click', () => {
 	}
 })
 
-
-
-
-// function burgerMenu(querySelector){
-//     let menu = $(selector);
-//     let button = menu.find('.burger-meny__button');
-//     let links = menu.find('burger-meny__link');
-//     let overlay = menu.find('burger-meny__overlay')
-
-//     button.on('click', (e) =>{
-//         e.preventDefault();
-//         toggleMenu();
-//     })
-//     links.on('click', ()=> toggleMenu());
-//     overlay.on('click', ()=> toggleMenu())
-
-//     function toggleMenu(){
-//         menu.toggleClass('menu-dtn_active');
-
-//         if (menu.hasClass('menu-dtn_active')){
-//             $('body').css('overflow', 'hidden');
-//         } else {
-//             $('body').css('overflow', 'visible');
-//         }
-//     }
-// }
-
-// burgerMenu('.burger-menu')
-
-// })(jQuery);
--- template_jq.js
+++ template_jq.js
@@ -1,5 +1,6 @@
 (function ($) {
   $(document).ready(function () {
+
 		$('.r-active').on('click', function () {
 			var list = $(this).parent().find('ul').fadeIn(250, function () {
 				$('body').one('click', function () {
@@ -7,5 +8,22 @@
 				});
 			});
 		});
+
+		$(".my-slider").slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			autoplay: true,
+			autoplaySpeed: 3000
+			
+		})
+		// if($(window).width()<1100){
+		// 	$('.my-slider').slick('unslick');
+		// }
+		
+
 	});
+
+	
 })(jQuery);
+
+
